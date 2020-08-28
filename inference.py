56d59d98f0dc9b3d6656cbb56b4715b56f23b04e
Karan Thakkar <kkt31415@gmail.com>
2020-08-28 13:21:40 +0530

    :ship: Added modes for inference

--- inference.py
+++ inference.py
@@ -9,6 +9,46 @@ from utils.hparams import HParam
 from dataset.text import text_to_sequence
 import numpy as np
 
+def sentence_to_phrases(line, words=3):
+    wordlist = line.split()
+    phrases = []
+    for i in range(0, len(wordlist), words):
+        if i + words > len(wordlist):
+            phrases[-1] = phrases[-1] +" "+' '.join(wordlist[i:i+words])
+        else:
+            phrases.append(' '.join(wordlist[i:i+words]))
+    return phrases
+
+def create_phrase_data(lines, words=4):
+    #
+    dataset=[]
+    for line in lines:
+        pre = []
+        current = []
+        post = []
+    if len(line.split()) < 8:
+        pre.append("^")
+        current.append(line)
+        post.append("~")
+    else:
+        phrases = sentence_to_phrases(line, words=words)
+      #print(phrases)
+        for i in range(0, len(phrases), 1):
+
+            if i == 0:
+                pre.append("^")
+            else:
+                pre.append(phrases[i-1])
+
+            current.append(phrases[i])
+
+            if i == len(phrases)-1:
+                post.append("~")
+                break
+            else:
+                post.append(phrases[i+1])
+    return pre, current, post
+
 def load_checkpoint(checkpoint_path, model):
     assert os.path.isfile(checkpoint_path)
     print("Loading checkpoint '{}'".format(checkpoint_path))
@@ -21,20 +61,43 @@ def load_checkpoint(checkpoint_path, model):
         checkpoint_path, iteration))
     return model
 
-def main(checkpoint_path, hparams, reference_mel, pre_text, text, post_text ):
+def process_input(text, mode):
+    dataset = []
+    if mode == 0:
+        pre = ["^"]
+        post = ["~"]
+        current = text.strip().split()
+    if mode == 1:
+        pre, current, post = create_phrase_data(text, 5)
+
+    for i, j, k in zip(pre, current, post):
+        dataset.append("{}|{}|{}".format(i,j,k))
+
+    return dataset
+
+
+def main(checkpoint_path, hparams, reference_mel, dataset, name ):
     model = PPSpeech(hparams, len(symbols)).cuda()
     model = load_checkpoint(checkpoint_path, model)
     model.eval()
-    input_text = torch.LongTensor(text_to_sequence(text, hparams.text_cleaners)).cuda().unsqueeze(0)
-    pre_text = torch.LongTensor(text_to_sequence(pre_text, hparams.text_cleaners)).cuda().unsqueeze(0)
-    post_text = torch.LongTensor(text_to_sequence(post_text, hparams.text_cleaners)).cuda().unsqueeze(0)
-    reference = torch.from_numpy(np.load(reference_mel)).cuda().unsqueeze(0)
-    with torch.no_grad():
-        print("predicting")
-        outs = model.inference(input_text, pre_text, post_text, style_input = reference)
-        mel = outs[0]
-        np.save("Mel.npy", mel.detach().cpu().numpy())
-    print("Mel generated")
+    output = []
+    for point in dataset:
+        input_text = torch.LongTensor(text_to_sequence(point[1], hparams.text_cleaners)).cuda().unsqueeze(0)
+        pre_text = torch.LongTensor(text_to_sequence(point[0], hparams.text_cleaners)).cuda().unsqueeze(0)
+        post_text = torch.LongTensor(text_to_sequence(point[2], hparams.text_cleaners)).cuda().unsqueeze(0)
+        reference = torch.from_numpy(np.load(reference_mel)).cuda().unsqueeze(0)
+        with torch.no_grad():
+            print("predicting")
+            outs = model.inference(input_text, pre_text, post_text, style_input = reference)
+            mel = outs[0]
+            output.append(mel)
+    if len(output)==1:
+        output_mel = output[0]
+    else:
+        output_mel = np.concatenate(output, axis = 1)
+
+    np.save(f"PPSpeech_{name}.npy", mel.detach().cpu().numpy())
+    print(f"Mel generated with name PPSpeech_{name}.npy ")
 
     return
 
@@ -44,14 +107,14 @@ if __name__ == '__main__':
                         help='Checkpoint path to read')
     parser.add_argument('-r', '--reference_mel', type=str,
                         help='Reference Mel for Acoustic Embedding')
-    parser.add_argument('-p', '--pre_text', type=str,
-                        help='Pre Text')
-    parser.add_argument('-cu', '--text', type=str,
-                        help='Current Text')
-    parser.add_argument('-po', '--post_text', type=str,
-                        help='Post Text')
+    parser.add_argument('-t', '--text', type=str,
+                        help='Input Text')
     parser.add_argument('--config', type=str,
-                        required=False, help='Config file')
+                        required=True, help='Config file')
+    parser.add_argument('-n','--name', type=str,
+                        required=True, help='Name of the file to be saved')
+    parser.add_argument('-m','--mode', type=int,
+                        required=True, help='Mode of inference: 0 - Sentence , 1 - Phrase ')
 
     args = parser.parse_args()
 
@@ -65,4 +128,5 @@ if __name__ == '__main__':
     print("cuDNN Enabled:", hp.cudnn_enabled)
     print("cuDNN Benchmark:", hp.cudnn_benchmark)
 
-    main(args.checkpoint_path, hp, args.reference_mel, args.pre_text, args.text, args.post_text )
+    dataset = process_input(args.text, args.mode)
+    main(args.checkpoint_path, hp, args.reference_mel, dataset , args.name)
